b5c4dd6a4833ee174f7e4a0f116eafef324d87bd
jayeshdalal <jayesh.dalal@go-jek.com>
2023-02-25 09:57:38 +0530

    [jayesh] - leet code [25/2/23]

--- Arrays/SearchInsertPosition.py
+++ Arrays/SearchInsertPosition.py
@@ -9,7 +9,7 @@ class SearchInsertPosition:
             mid = int(start + (end - start) / 2)
             if nums[mid] == target:
                 return mid
-            elif nums[mid] <target:
+            elif nums[mid] < target:
                 start = mid + 1
             else:
                 end = mid - 1
@@ -17,4 +17,4 @@ class SearchInsertPosition:
 
 if __name__ == '__main__':
     si = SearchInsertPosition
-    print(si.solution(si, [1,3,5,6], 5))
+    print(si.solution(si, [1,3,5,6], 6))
