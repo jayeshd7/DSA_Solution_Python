5d0354c23c08e2d3655099261a10d21086a4d212
jayesh <jayeshdalal7@gmail.com>
2023-05-19 11:59:32 +0530

    [jayesh] - Adding Enum concept in python.

---
+++ oops/enum_interface.py
@@ -0,0 +1,17 @@
+
+from enum import Enum
+class DayOfWeek(Enum):
+    MONDAY = 1
+    TUESDAY = 2
+    WEDNESDAY =3
+    THURSDAY = 4
+    FRIDAY = 5
+    SATURDAY = 6
+    SUNDAY = 7
+
+today = DayOfWeek.FRIDAY
+print(today)
+
+
+for day in DayOfWeek:
+    print(day.name, day.value)
---
+++ useful_python_concepts/useful_tricks.py
@@ -0,0 +1,12 @@
+# 1. Tuple unpacking
+
+person = ["bob", 30, "male"]
+name, age, gender = person
+
+fruits = ["apple", "orange", "pear", "pineapple", "durian", "banana"]
+
+first, second, *others = fruits
+
+# 2.List /Dict compressive
+
+
